a8bc88a1aa7331aa19a2bd714c68f4c13f25fa5e
Leopoldo Pla <leopoldoplasempere@gmail.com>
2013-12-15 18:21:00 +0100

    Añadido método para imprimir por pantalla puntos de dos clases y una función

--- Utils.h
+++ Utils.h
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <fstream>
 #include "float.h"
+#include <boost/algorithm/string.hpp>
 
 class Utils{
 public:
@@ -44,7 +45,25 @@ public:
 		for(int i = 0; i < x.size(); i++)
 			myfile << x[i] << " " << y[i] << std::endl;
 		myfile.close();
-		system("gnuplot -persist -e \"plot './datafile.dat' using 1:2 title 'X' with linespoints, './datafile.dat' using 1:2 title 'Y' with points\"");
+		system("gnuplot -persist -e \"plot './datafile.dat' using 1:2 title 'Point'\"");
+
+	}
+
+	static void plotDataFunction(std::vector<double> x1, std::vector<double> y1, std::vector<double> x2, std::vector<double> y2, std::string function){
+		boost::replace_all(function, "^", "**");
+
+		std::ofstream myfile;
+		myfile.open ("dataset1.dat");
+		for(int i = 0; i < x1.size(); i++)
+			myfile << x1[i] << " " << y1[i] << std::endl;
+		myfile.close();
+		myfile.open ("dataset2.dat");
+		for(int i = 0; i < x2.size(); i++)
+			myfile << x2[i] << " " << y2[i] << std::endl;
+		myfile.close();
+		std::string command = "gnuplot -persist -e \"plot './datafile1.dat' using 1:2 title 'Points dataset 1', './datafile2.dat' using 1:2 title 'Points dataset 2', " + function + "\"";
+		system(command.c_str());
+
 	}
 
 	static void doShit(){
